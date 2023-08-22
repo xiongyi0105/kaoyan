eb7aee974f359f824e660eb2b3dd78e16298da7e
xiongyi0105 <1019487236@qq.com>
2023-08-22 12:48:57 +0800

    edit bst operations

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 typedef struct BSTNode {
     int data;
-    struct BSTNode *lchild, *rchild;
+    struct BSTNode *l_child, *r_child;
 } BSTNode, *BST;
 
 // 插入节点
@@ -14,57 +14,58 @@ void CreateBST(BST *, int[], int);
 BSTNode *BSTSearch(BST, int);
 
 
-int BSTInsert(BST *bstP, int d) {
+int BSTInsert(BST *bst_pointer, int d) {
     // 出口
     // 找到NULL节点时，创建新节点，加入二叉排序树，并返回1
-    if (*bstP == NULL) {
-        *bstP = (BSTNode *) malloc(sizeof(BSTNode));
-        (*bstP)->data = d;
-        (*bstP)->lchild = NULL;
-        (*bstP)->rchild = NULL;
+    if (*bst_pointer == NULL) {
+        *bst_pointer = (BSTNode *) malloc(sizeof(BSTNode));
+        (*bst_pointer)->data = d;
+        (*bst_pointer)->l_child = NULL;
+        (*bst_pointer)->r_child = NULL;
         return 1;
     }
         // 公式
         // 所指节点与待插入数相等时 返回0；所指节点小于待插入数时，向右子树插入。反之向左子树插入。
-    else if ((*bstP)->data == d) {
+    else if ((*bst_pointer)->data == d) {
         return 0;
-    } else if ((*bstP)->data < d) {
-        return BSTInsert(&(*bstP)->rchild, d);
+    } else if ((*bst_pointer)->data < d) {
+        return BSTInsert(&(*bst_pointer)->r_child, d);
     } else {
-        return BSTInsert(&(*bstP)->lchild, d);
+        return BSTInsert(&(*bst_pointer)->l_child, d);
     }
 }
 
-void CreateBST(BST *bstP, int data[], int n) {
+void CreateBST(BST *bst_pointer, int data[], int n) {
     // 向一颗空树中按数组序列循环添加节点。
-    *bstP = NULL;
+    *bst_pointer = NULL;
     int i = 0;
     while (i < n) {
-        BSTInsert(bstP, data[i]);
+        BSTInsert(bst_pointer, data[i]);
         i++;
     }
 }
 
-BSTNode *BSTSearch(BST p, int data) {
-    while (p != NULL && p->data != data) {
-        if (p->data < data) {
-            p = p->rchild;
+BSTNode *BSTSearch(BST bst, int data) {
+    // 循环比对值，比对成功或为NULL时退出
+    while (bst != NULL && bst->data != data) {
+        if (bst->data < data) {
+            bst = bst->r_child;
         } else {
-            p = p->lchild;
+            bst = bst->l_child;
         }
 
     }
-    return p;
+    return bst;
 }
 
 int main(int argc, char const *argv[]) {
     int n = 5;
     int data[] = {4, 7, 6, 5, 10};
-    BST *BstRootP = (BST *) malloc(sizeof(BST *));
-    CreateBST(BstRootP, data, n);
-    BSTNode *found = BSTSearch(*BstRootP, 121);
-    if (found) {
-        printf("%d", found->data);
+    BST *bst_root_pointer = (BST *) malloc(sizeof(BST *));
+    CreateBST(bst_root_pointer, data, n);
+    BSTNode *found_bst = BSTSearch(*bst_root_pointer, 121);
+    if (found_bst) {
+        printf("%d", found_bst->data);
 
     } else {
         printf("NULL");
