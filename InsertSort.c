b2c6452349bc75f6f860ccc45e369ed593f43105
xiongyi0105 <1019487236@qq.com>
2023-08-27 16:14:53 +0800

    BST include limits.h of INT_MAX and INT_MIN

--- InsertSort.c
+++ InsertSort.c
@@ -3,14 +3,18 @@
 //
 #include <stdio.h>
 
-void InsertSort(int [], int);
+void InsertSort(int[], int);
 
-void InsertSort(int A[], int n) {
+void InsertSort(int A[], int n)
+{
     int i, j, temp;
-    for (i = 1; i < n; i++) {
-        if (A[i] < A[i - 1]) {
+    for (i = 1; i < n; i++)
+    {
+        if (A[i] < A[i - 1])
+        {
             temp = A[i];
-            for (j = i - 1; j >= 0 && A[j] > temp; j--) {
+            for (j = i - 1; j >= 0 && A[j] > temp; j--)
+            {
                 A[j + 1] = A[j];
             }
             A[j + 1] = temp;
@@ -18,39 +22,41 @@ void InsertSort(int A[], int n) {
     }
 }
 
-void BinaryInsertSort(int A[], int n) {
+void BinaryInsertSort(int A[], int n)
+{
     int i, j, low, high, mid, temp;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++)
+    {
         temp = A[i];
         low = 0;
         high = i - 1;
-        while (low <= high) {
+        while (low <= high)
+        {
             mid = (low + high) / 2;
-            if (A[mid] > temp) {
+            if (A[mid] > temp)
+            {
                 high = mid - 1;
-            } else {
+            }
+            else
+            {
                 low = mid + 1;
             }
         }
-        for (j = i - 1; j >= low; j--) {
+        for (j = i - 1; j >= low; j--)
+        {
             A[j + 1] = A[j];
         }
         A[low] = temp;
-
     }
-
-
 }
 
-int main() {
+int main()
+{
     int A[] = {49, 38, 65, 97, 76, 13, 27, 49};
-//    InsertSort(A,8);
+    //    InsertSort(A,8);
     BinaryInsertSort(A, 8);
-    for (int i = 0; i < sizeof(A) / sizeof(int); i++) {
+    for (int i = 0; i < sizeof(A) / sizeof(int); i++)
+    {
         printf("%d ", A[i]);
     }
 }
-
-
-
-
--- ShellSort.c
+++ ShellSort.c
@@ -3,31 +3,35 @@
 //
 #include <stdio.h>
 
-void ShellSort(int [], int);
+void ShellSort(int[], int);
 
-void ShellSort(int A[], int n) {
+void ShellSort(int A[], int n)
+{
     int i, j, d, temp;
     // d为步长（增量）针对不同的步长依次对子列排序，直到步长为1时排序完成
     for (d = n / 2; d >= 1; d = d / 2)
         // 找到按步长分布的第二个元素开始直插排序
-        for (i = d; i <= n; i++) {
-            if (A[i] < A[i - d]) {
+        for (i = d; i <= n; i++)
+        {
+            if (A[i] < A[i - d])
+            {
                 temp = A[i];
                 // 符合条件时，将大于待插入数的元素依次后移
-                for (j = i - d; j >= 0 && A[j] > temp; j -= d) {
+                for (j = i - d; j >= 0 && A[j] > temp; j -= d)
+                {
                     A[j + d] = A[j];
                 }
                 A[j + d] = temp;
             }
         }
-
 }
 
-
-int main() {
+int main()
+{
     int A[] = {49, 38, 65, 97, 76, 13, 27, 49};
     ShellSort(A, 8);
-    for (int i = 0; i < sizeof(A) / sizeof(int); i++) {
+    for (int i = 0; i < sizeof(A) / sizeof(int); i++)
+    {
         printf("%d ", A[i]);
     }
 }
